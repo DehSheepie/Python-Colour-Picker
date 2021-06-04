dae1303ecce0f7ff11b9fe0c569eef4cdba860e7
Sheepie <chrisparsonsm97@yahoo.co.uk>
2021-06-04 12:01:27 +0100

    Made it possible to remove colours from the list and made the colour list automatically update.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import json
+import os
 
 
 # saved for later hehe ^-^
@@ -11,24 +12,6 @@ def hex_to_rgb(hex_string) -> (int, int, int):
     g = int(string[2] + string[3], 16)
     b = int(string[4] + string[5], 16)
     return r, g, b
-
-
-def add_to_file(hex_string):
-    colours = None
-    with open("colour_file.json") as file:
-        try:
-            data = json.load(file)
-            try:
-                colours = data
-                colours["list"].append(hex_string)
-            except ValueError:
-                print(f"Exception {ValueError}")
-                colours = {"list": [hex_string]}
-        except:
-            colours = {"list": [hex_string]}
-
-    with open("colour_file.json", "w") as file:
-        json.dump(colours, file)
 
 
 class ColourPickerWindow:
@@ -45,7 +28,8 @@ class ColourPickerWindow:
         self.top_section.pack(side=tk.TOP)
 
         # Colour Display
-        self.colour_display = tk.Canvas(self.top_section, width=300, height=100, bg='#666', bd=1, highlightbackground="black")
+        self.colour_display = tk.Canvas(self.top_section, width=300, height=100, bg='#666', bd=1,
+                                        highlightbackground="black")
         self.colour_display.pack(pady=20, side=tk.TOP)
 
         # Hex Value
@@ -117,19 +101,35 @@ class ColourPickerWindow:
         self.bottom_section.pack(side=tk.TOP)
 
         self.store_colour = tk.Button(self.bottom_section, text="Store Colour",
-                                      command=lambda: add_to_file(self.colour_display["background"]))
-        self.store_colour.pack()
+                                      command=lambda: self.add_to_file(self.colour_display["background"]))
+        self.store_colour.pack(pady=10)
 
-        self.display_all_colours = tk.Button(self.bottom_section, text="Display Colours",
-                                             command=lambda :self.display_colours())
-        self.display_all_colours.pack(pady=10)
-
-        self.display_stored_colours = tk.Canvas(self.bottom_section, width=300, height=100, bg='#fff', bd=1, highlightbackground="black")
+        self.display_stored_colours = tk.Canvas(self.bottom_section, width=300, height=100, bg='#fff', bd=1,
+                                                highlightbackground="black")
         self.display_stored_colours.pack(side=tk.TOP)
 
         self.display_colours()
         # Starts the window running
         self.window.mainloop()
+
+    def add_to_file(self, hex_string):
+        colours = None
+        if not os.path.exists("colour_file.json"):
+            file = open("colour_file.json", "w+")
+        with open("colour_file.json") as file:
+            try:
+                data = json.load(file)
+                try:
+                    colours = data
+                    colours["list"].append(hex_string)
+                except ValueError:
+                    print(f"Exception {ValueError}")
+                    colours = {"list": [hex_string]}
+            except:
+                colours = {"list": [hex_string]}
+        with open("colour_file.json", "w") as file:
+            json.dump(colours, file)
+        self.display_colours()
 
     def preview_colour(self, red: int = 0, green: int = 0, blue: int = 0):
         colour = f"#{int(red):02x}{int(green):02x}{int(blue):02x}"
@@ -148,31 +148,51 @@ class ColourPickerWindow:
 
     def preview_hex_colour(self, hex_string):
         colour = hex_to_rgb(hex_string)
-        self.preview_colour(colour[0], colour[1], colour[2])
+        self.preview_colour(colour[0], colour[1], colour[2])  #
+
+    def remove_hex_colour(self, hex_string):
+        with open("colour_file.json", 'r') as file:
+            data = json.load(file)
+            colours = data
+            colours["list"].remove(hex_string)
+
+        with open("colour_file.json", 'w') as file:
+            file.write(json.dumps(colours))
+
+        self.display_colours()
 
     def display_colours(self):
-        with open("colour_file.json") as file:
+        self.display_stored_colours.delete("all")
+        if os.path.exists("colour_file.json"):
+            with open("colour_file.json") as file:
+                try:
+                    data = json.load(file)
 
-            data = json.load(file)
-
-            index = 0
-            xpos = 0
-            ypos = 0
-            for row in range(4):
-                for col in range(12):
-                    if index < len(data["list"]):
-                        rect = self.display_stored_colours.create_rectangle(xpos,ypos,xpos+25,ypos+25, fill=data["list"][index])
-                        self.display_stored_colours.tag_bind(rect, "<Button-1>", lambda event, hex=self.display_stored_colours.itemcget(rect, "fill"): self.preview_hex_colour(hex))
-                        #)
-                        index +=1
-                    xpos+=25
-                xpos=0
-                ypos+=25
-
-
-
-
+                    index = 0
+                    xpos = 0
+                    ypos = 0
+                    for row in range(4):
+                        for col in range(12):
+                            if index < len(data["list"]):
+                                rect = self.display_stored_colours.create_rectangle(xpos, ypos, xpos + 25, ypos + 25,
+                                                                                    fill=data["list"][index])
+                                self.display_stored_colours.tag_bind(rect, "<Button-1>", lambda event,
+                                                                                                hex=self.display_stored_colours.itemcget(
+                                                                                                    rect,
+                                                                                                    "fill"): self.preview_hex_colour(
+                                    hex))
+                                self.display_stored_colours.tag_bind(rect, "<Button-3>", lambda event,
+                                                                                                hex=self.display_stored_colours.itemcget(
+                                                                                                    rect,
+                                                                                                    "fill"): self.remove_hex_colour(
+                                    hex))
+                                index += 1
+                            xpos += 25
+                        xpos = 0
+                        ypos += 25
+                except:
+                    pass
 
 
 print(int("f", 16))
-ColourPickerWindow()
+value = ColourPickerWindow()
